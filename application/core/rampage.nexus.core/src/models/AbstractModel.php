e136cd9591df0e57cf0b7df41f04f0308a9e9fe2
tux-rampage <staff@pro-unreal.de>
2013-06-27 22:52:20 +0200

    Initial implementation

---
+++ application/core/rampage.nexus.core/src/models/AbstractModel.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * This is part of rampage.php
+ * Copyright (c) 2013 Axel Helmert
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category  library
+ * @package   rampage.nexus
+ * @author    Axel Helmert
+ * @copyright Copyright (c) 2013 Axel Helmert
+ * @license   http://www.gnu.org/licenses/gpl-3.0.txt GNU General Public License
+ */
+
+namespace rampage\nexus\core\models;
+
+/**
+ * Abstact model
+ */
+class AbstractModel
+{
+    /**
+     * @orm:field()
+     * @var int
+     */
+    private $id = null;
+
+    /**
+     * @return number
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
